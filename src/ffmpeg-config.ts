cd850c60964af3943341ea9a0b31f9c98ca5591b
Jens Fursund <jens@fursund.com>
2025-11-05 09:58:50 +0100

    Revert "Point to a file that exists"
    This reverts commit dba63b87d137fdbdf324e71c0451aa93abdc8b6d.

--- src/ffmpeg-config.ts
+++ src/ffmpeg-config.ts
@@ -1,5 +1,5 @@
 export default {
   'lgpl-base':
     'https://unpkg.com/@diffusion-studio/ffmpeg-lgpl-base@0.0.1/dist/ffmpeg-core.js',
-  'gpl-extended': 'https://unpkg.com/@ffmpeg/core@0.12.10/dist/esm/ffmpeg-core.js',
+  'gpl-extended': 'https://unpkg.com/@ffmpeg/core@0.12.6/dist/ffmpeg-core.js',
 };
